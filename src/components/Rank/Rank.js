4a48b3c5a2a4e53f46757583f9c348c2d6ac60b6
axon4 <65050809+axon4@users.noreply.github.com>
2020-05-25 18:35:29 +0100

    initial-commit

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+
+const Rank = () => {
+    return (
+        <Fragment>
+            <div className='white f3'>
+                {'Axon, your current rank is...'}
+            </div>
+            <div className='white f1'>
+                {'#7'}
+            </div>
+        </Fragment>
+    );
+};
+
+export default Rank;
