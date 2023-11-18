26b43a2d927fa64a0c89df0488e6043d5fb55c26
axon4 <65050809+axon4@users.noreply.github.com>
2023-11-18 20:35:21 +0000

    delete alt-attribute from inPut-image

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -5,7 +5,7 @@ const FaceRecognition = ({ imageURL, boxes }) => {
 	return (
 		<div className='centre ma'>
 			<div className='absolute mt2'>
-				<img id='inPutImage' src={imageURL} alt='submitted image' width='500px' height='auto' />
+				<img id='inPutImage' src={imageURL} width='500px' height='auto' />
 				{boxes.map(box => <div key={box.rightColumn} style={{top: box.topRow, right: box.rightColumn, bottom: box.bottomRow, left: box.leftColumn}} className='bounding-box' />)}
 			</div>
 		</div>
